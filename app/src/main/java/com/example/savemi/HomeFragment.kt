bf2ea70cee531da8aebc4b9ab4824a7471b98fe9
MathiasThyge <thyge97@gmail.com>
2020-06-08 18:00:18 +0200

    Det er nu muligt at navigere mellem tre sider.
    HomeFragment - hvor brugeren ser alle side informationer
    
    LoginFragment - den allerførste side brugeren møder
    RegestrerFragment - siden hvor brugeren skal oprette sig selv

--- app/src/main/java/com/example/savemi/HomeFragment.kt
+++ app/src/main/java/com/example/savemi/HomeFragment.kt
@@ -5,31 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass.
  */
-class LoginFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-            return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_home,container,false)
         }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.login_button).setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_homefragment)
-        }
-
-        view.findViewById<TextView>(R.id.createaccounttext).setOnClickListener{
-            findNavController().navigate(R.id.action_loginFragment_to_registrerFragment)
+        view.findViewById<Button>(R.id.logout_button).setOnClickListener {
+            findNavController().navigate(R.id.action_homefragment_to_loginfragment)
         }
     }
 
---
+++ app/src/main/java/com/example/savemi/RegistrerFragment.kt
@@ -0,0 +1,26 @@
+package com.example.savemi
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class RegistrerFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_registrer, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
